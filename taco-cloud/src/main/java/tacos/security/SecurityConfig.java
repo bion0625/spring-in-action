c5a73f7a8cd407f5c65a4a372408d7369ce3f4d6
이의정 <stxtory@gmail.com>
2024-12-04 19:16:29 +0900

    4.2.2 JDBC 기반의 사용자 스토어

--- taco-cloud/src/main/java/tacos/security/SecurityConfig.java
+++ taco-cloud/src/main/java/tacos/security/SecurityConfig.java
@@ -1,23 +1,30 @@
 package tacos.security;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+	
+	@Autowired
+	private DataSource dataSource;
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		return http
+				.csrf(csrf -> csrf.ignoringRequestMatchers("/h2-console/**"))
+				.headers(header -> header.frameOptions(f -> f.disable()))
 		.authorizeHttpRequests(auth -> auth
 				.requestMatchers("/design", "/orders").hasRole("USER")
 				.anyRequest().permitAll())
@@ -27,16 +34,14 @@ public class SecurityConfig {
 	
 	@Bean
 	public UserDetailsService users() {
-		UserDetails user1 = User.builder()
-				.username("user1")
-				.password("{noop}password1")
-				.roles("USER")
-				.build();
-		UserDetails user2 = User.builder()
-				.username("user2")
-				.password("{noop}password2")
-				.roles("USER")
-				.build();
-		return new InMemoryUserDetailsManager(user1, user2);
+		JdbcUserDetailsManager detailsManager = new JdbcUserDetailsManager(dataSource);
+		detailsManager.setUsersByUsernameQuery("select username, password, enabled from users where username = ?");
+		detailsManager.setAuthoritiesByUsernameQuery("select username, authority from authorities where username = ?");
+		return detailsManager;
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new NoEncodingPasswordEncoder();
 	}
 }
