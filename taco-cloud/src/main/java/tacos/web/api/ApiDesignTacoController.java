caf26f2ab8510c403d8f7c1f43a17d4878404c8e
이의정 <stxtory@gmail.com>
2024-12-05 21:36:55 +0900

    6.1.2 서버에 데이터 전송하기

--- taco-cloud/src/main/java/tacos/web/api/ApiDesignTacoController.java
+++ taco-cloud/src/main/java/tacos/web/api/ApiDesignTacoController.java
@@ -10,7 +10,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import tacos.Taco;
@@ -39,5 +42,11 @@ public class ApiDesignTacoController {
 		return tacoRepo.findById(id)
 				.map(taco -> new ResponseEntity<>(taco, HttpStatus.OK))
 				.orElse(new ResponseEntity<>(null, HttpStatus.NOT_FOUND));
+	}
+	
+	@PostMapping(consumes = "application/json")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Taco postTaco(@RequestBody Taco taco) {
+		return tacoRepo.save(taco);
 	}
 }
