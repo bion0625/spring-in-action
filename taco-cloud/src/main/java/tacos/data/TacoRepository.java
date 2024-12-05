d0a53cf581da2ef6c86713f77f17607edc27dd72
bion0625 <bion0625@gmail.com>
2024-12-05 16:38:15 +0900

    6.1.1 서버에서 데이터 가져오기

--- taco-cloud/src/main/java/tacos/data/TacoRepository.java
+++ taco-cloud/src/main/java/tacos/data/TacoRepository.java
@@ -1,8 +1,8 @@
 package tacos.data;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import tacos.Taco;
 
-public interface TacoRepository extends CrudRepository<Taco, Long> {
+public interface TacoRepository extends JpaRepository<Taco, Long> {
 }
---
+++ taco-cloud/src/main/java/tacos/web/api/ApiDesignTacoController.java
@@ -0,0 +1,43 @@
+package tacos.web.api;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import tacos.Taco;
+import tacos.data.TacoRepository;
+
+@RestController
+@RequestMapping(path = "/design", produces = "application/json")
+@CrossOrigin(origins = "*")
+public class ApiDesignTacoController {
+	private final TacoRepository tacoRepo;
+	
+//	EntityLinks
+	
+	public ApiDesignTacoController(TacoRepository tacoRepo) {
+		this.tacoRepo = tacoRepo;
+	}
+	
+	@GetMapping("/recent")
+	public Iterable<Taco> recentTacos() {
+		PageRequest page = PageRequest.of(0, 12, Sort.by("createdAt").descending());
+		return tacoRepo.findAll(page).getContent();
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Taco> tacoById(@PathVariable Long id) {
+		return tacoRepo.findById(id)
+				.map(taco -> new ResponseEntity<>(taco, HttpStatus.OK))
+				.orElse(new ResponseEntity<>(null, HttpStatus.NOT_FOUND));
+	}
+}
