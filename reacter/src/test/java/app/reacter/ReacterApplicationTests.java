d34153e2ffd7d688112efbd304f15b6f475c60eb
bion0625 <bion0625@gmail.com>
2024-12-09 18:37:33 +0900

    10.3.1 리액티브 타입 생성하기

--- reacter/src/test/java/app/reacter/ReacterApplicationTests.java
+++ reacter/src/test/java/app/reacter/ReacterApplicationTests.java
@@ -1,13 +1,112 @@
 package app.reacter;
 
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
 @SpringBootTest
 class ReacterApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	void createAFlux_just() {
+		Flux<String> fruitFlux = Flux
+				.just("Apple", "Orange", "Grape", "Banana", "Strawberry");
+//		fruitFlux.subscribe(f -> System.out.println("Here's some fruit: " + f));
+		
+		StepVerifier.create(fruitFlux)
+		.expectNext("Apple")
+		.expectNext("Orange")
+		.expectNext("Grape")
+		.expectNext("Banana")
+		.expectNext("Strawberry")
+		.verifyComplete();
+	}
+	
+	@Test
+	void createAFlux_fromArray() {
+		String[] fruits = new String[] {"Apple", "Orange", "Grape", "Banana", "Strawberry"};
+		
+		Flux fruitFlux = Flux.fromArray(fruits);
+		
+		StepVerifier.create(fruitFlux)
+		.expectNext("Apple")
+		.expectNext("Orange")
+		.expectNext("Grape")
+		.expectNext("Banana")
+		.expectNext("Strawberry")
+		.verifyComplete();
+	}
+	
+	@Test
+	void createAFlux_fromIterable() {
+		List<String> fruitList = new ArrayList<>();
+		fruitList.add("Apple");
+		fruitList.add("Orange");
+		fruitList.add("Grape");
+		fruitList.add("Banana");
+		fruitList.add("Strawberry");
+		
+		Flux fruitFlux = Flux.fromIterable(fruitList);
+		
+		StepVerifier.create(fruitFlux)
+		.expectNext("Apple")
+		.expectNext("Orange")
+		.expectNext("Grape")
+		.expectNext("Banana")
+		.expectNext("Strawberry")
+		.verifyComplete();
+	}
+	
+	@Test
+	void createAFlux_fromStream() {
+		Stream<String> fruitStream = Stream.of("Apple", "Orange", "Grape", "Banana", "Strawberry");
+		
+		Flux fruitFlux = Flux.fromStream(fruitStream);
+		
+		StepVerifier.create(fruitFlux)
+		.expectNext("Apple")
+		.expectNext("Orange")
+		.expectNext("Grape")
+		.expectNext("Banana")
+		.expectNext("Strawberry")
+		.verifyComplete();
+	}
+	
+	@Test
+	void createAFlux_range() {
+		Flux<Integer> intervalFlux = Flux.range(1, 5);
+		
+		StepVerifier.create(intervalFlux)
+		.expectNext(1)
+		.expectNext(2)
+		.expectNext(3)
+		.expectNext(4)
+		.expectNext(5)
+		.verifyComplete();
+	}
+	
+	@Test
+	void ctreateAFlux_interval() {
+		Flux<Long> intervalFlux = Flux.interval(Duration.ofSeconds(1)).take(5);
+		
+		StepVerifier.create(intervalFlux)
+		.expectNext(0L)
+		.expectNext(1L)
+		.expectNext(2L)
+		.expectNext(3L)
+		.expectNext(4L)
+		.verifyComplete();
 	}
 
 }
