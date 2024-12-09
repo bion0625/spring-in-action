047b650f9389bbeb67e1f7d86ebc93b452c903b2
bion0625 <bion0625@gmail.com>
2024-12-09 17:38:32 +0900

    10.2.2 리액터 의존성 추가하기

---
+++ reacter/src/main/java/app/reacter/ReacterApplication.java
@@ -0,0 +1,13 @@
+package app.reacter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReacterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReacterApplication.class, args);
+	}
+
+}
