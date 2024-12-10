4c1b2b8368e75344a75f0bc130a31d2d03abc2a0
bion0625 <bion0625@gmail.com>
2024-12-10 15:18:22 +0900

    10.3.4 리액티브 타입에 로직 오퍼레이션 수행하기

--- reacter/src/test/java/app/reacter/ReacterApplicationTests.java
+++ reacter/src/test/java/app/reacter/ReacterApplicationTests.java
@@ -345,4 +345,36 @@ class ReacterApplicationTests {
 				&& map.get('k').equals("kangaroo"))
 			.verifyComplete();
 	}
+	
+	@Test
+	void all() {
+		Flux<String> animalFlux = Flux.just(
+				"aardvark", "elephant", "koala", "eagle", "kangaroo");
+
+		Mono<Boolean> hasAMono = animalFlux.all(a -> a.contains("a"));
+		StepVerifier.create(hasAMono)
+		.expectNext(true)
+		.verifyComplete();
+		
+		Mono<Boolean> hasKMono = animalFlux.all(a -> a.contains("k"));
+		StepVerifier.create(hasKMono)
+		.expectNext(false)
+		.verifyComplete();
+	}
+	
+	@Test
+	void any() {
+		Flux<String> animalFlux = Flux.just(
+				"aardvark", "elephant", "koala", "eagle", "kangaroo");
+		
+		Mono<Boolean> hasTMono = animalFlux.any(a -> a.contains("t"));
+		StepVerifier.create(hasTMono)
+		.expectNext(true)
+		.verifyComplete();
+		
+		Mono<Boolean> hasZMono = animalFlux.any(a -> a.contains("z"));
+		StepVerifier.create(hasZMono)
+		.expectNext(false)
+		.verifyComplete();
+	}
 }
