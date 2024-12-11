90a2978e44cad2cbfebf3d47a60e7fa096a5187e
bion0625 <bion0625@gmail.com>
2024-12-11 18:34:45 +0900

    11.5.2 리액티브 사용자 명세 서비스 구성하기

--- reacter/src/main/java/app/reacter/SecurityConfig.java
+++ reacter/src/main/java/app/reacter/SecurityConfig.java
@@ -1,10 +1,18 @@
 package app.reacter;
 
+import java.util.Collection;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -17,5 +25,35 @@ public class SecurityConfig {
 				.pathMatchers("/design", "/orders").authenticated()
 				.anyExchange().permitAll());
 		return http.build();
+	}
+	
+	@Bean
+	ReactiveUserDetailsService userDetailsService() {
+		return new ReactiveUserDetailsService() {
+			
+			@Override
+			public Mono<UserDetails> findByUsername(String username) {
+				// TODO Auto-generated method stub
+				return Mono.just(username).map(name -> {
+					return new UserDetails() {
+						
+						@Override
+						public String getUsername() {
+							return name;
+						}
+						
+						@Override
+						public String getPassword() {
+							return name;
+						}
+						
+						@Override
+						public Collection<? extends GrantedAuthority> getAuthorities() {
+							return null;
+						}
+					};
+				});
+			}
+		};
 	}
 }
