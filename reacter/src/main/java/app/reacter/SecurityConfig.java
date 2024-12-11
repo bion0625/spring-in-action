bd6f138291077ce62c0f2b2ef9d1bee3d7225359
bion0625 <bion0625@gmail.com>
2024-12-11 18:00:32 +0900

    11.5.1 리액티브 웹 보안 구성하기

---
+++ reacter/src/main/java/app/reacter/SecurityConfig.java
@@ -0,0 +1,21 @@
+package app.reacter;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+	@Bean
+	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+		http.authorizeExchange(
+				exchange -> exchange
+				.pathMatchers("/design", "/orders").authenticated()
+				.anyExchange().permitAll());
+		return http.build();
+	}
+}
